617e8b9e26c80c096f0c90785afd18548bac0d65
Samal2002 <gouravranjansamal@gmail.com>
2025-03-07 17:00:00 +0530

    Initial commit

---
+++ delivery/admin.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from .models import Customer, Restaurant, Menu
+
+# Register your models here.
+admin.site.register(Customer)
+admin.site.register(Restaurant)
+admin.site.register(Menu)
+
+
